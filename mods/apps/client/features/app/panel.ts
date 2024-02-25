56152a4142464251ccc5906d5973bed4bda21384
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2024-02-25 19:27:13 +0100

    Improved panel swipe

--- mods/apps/client/features/app/panel.ts
+++ mods/apps/client/features/app/panel.ts
@@ -5,7 +5,7 @@ export function clamp(value: number, min: number, max: number) {
 }
 
 export function createPanel(nodes: Node[]) {
-  const $root = div_nodes("panel", nodes.map((node) => div_nodes("panel_item", [node])));
+  const $root = div_nodes("panel _transition", nodes.map((node) => div_nodes("panel_item", [node])));
 
   let startX = 0;
   let currentX = 0;
@@ -14,21 +14,21 @@ export function createPanel(nodes: Node[]) {
 
   $root.addEventListener("pointerdown", (event) => {
     startX = event.clientX;
-    currentX = event.clientX;
+    currentX = 0;
+    $root.classList.remove("_transition");
   });
   $root.addEventListener("pointerup", (event) => {
-    if (Math.abs(currentX) > 20) {
+    if (Math.abs(currentX) > 50) {
       parallax += currentX > 0 ? 1 : -1
       parallax = clamp(parallax, 0, total);
-      document.documentElement.style.setProperty("--animate-parallax-current", parallax.toString());
     }
+    document.documentElement.style.setProperty("--animate-parallax-current", parallax.toString());
+    $root.classList.add("_transition");
   });
   $root.addEventListener("pointermove", (event) => {
     currentX = startX - event.clientX;
-    if (Math.abs(currentX) > 20) {
-      const ele = parallax + (currentX / 500);
-      document.documentElement.style.setProperty("--animate-parallax-current", ele.toString());
-    }
+    const ele = parallax + (currentX / 500);
+    document.documentElement.style.setProperty("--animate-parallax-current", ele.toString());
   });
   return $root;
 }
