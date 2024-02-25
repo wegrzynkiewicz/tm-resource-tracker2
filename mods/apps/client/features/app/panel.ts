6d5bfad0a58031c685fb1438cbac3deb5dbcc703
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2024-02-25 18:46:07 +0100

    Add mobile panel swipe

--- mods/apps/client/features/app/panel.ts
+++ mods/apps/client/features/app/panel.ts
@@ -1,15 +1,34 @@
 import { div_nodes } from "../../../../common/frontend-framework/dom.ts";
 
+export function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
 export function createPanel(nodes: Node[]) {
   const $root = div_nodes("panel", nodes.map((node) => div_nodes("panel_item", [node])));
+
+  let startX = 0;
+  let currentX = 0;
+  let parallax = 0;
+  const total = 2;
+
   $root.addEventListener("pointerdown", (event) => {
-    console.log("panel pointerdown", event);
+    startX = event.clientX;
+    currentX = event.clientX;
   });
   $root.addEventListener("pointerup", (event) => {
-    console.log("panel pointerup", event);
+    if (Math.abs(currentX) > 20) {
+      parallax += currentX > 0 ? 1 : -1
+      parallax = clamp(parallax, 0, total);
+      document.documentElement.style.setProperty("--animate-parallax-current", parallax.toString());
+    }
   });
   $root.addEventListener("pointermove", (event) => {
-    console.log("panel pointermove", event);
+    currentX = startX - event.clientX;
+    if (Math.abs(currentX) > 20) {
+      const ele = parallax + (currentX / 500);
+      document.documentElement.style.setProperty("--animate-parallax-current", ele.toString());
+    }
   });
   return $root;
 }
